0f2cac3c9c99c77ea3caf14e7d22f263b2692546
Nigelo <vectorcrael@gmail.com>
2024-11-29 14:34:42 +0200

    Match to database for stock adjustments

--- DataLayer/Models/ZRASTockAdjustment.cs
+++ DataLayer/Models/ZRASTockAdjustment.cs
@@ -7,24 +7,24 @@ public class ZRASTockAdjustment
 {
     public string? CustTpin { get; set; }
     public string? CustNm { get; set; }
-    public string RegTyCd { get; set; }
+    public string? RegTyCd { get; set; }
     public string SarTyCd { get; set; }
     public decimal? TotTaxblAmt { get; set; }
-    public decimal? TotTaxAmt { get; set; }
-    public decimal? TotAmt { get; set; }
-    public string OcrnDt { get; set; }
+    public float? TotTaxAmt { get; set; }
+    public float? TotAmt { get; set; }
+    public string? OcrnDt { get; set; }
     public int ItemSeq { get; set; }
-    public string ItemCd { get; set; }
-    public string ItemClsCd { get; set; }
-    public string ItemNm { get; set; }
-    public string PkgUnitCd { get; set; }
+    public string? ItemCd { get; set; }
+    public string? ItemClsCd { get; set; }
+    public string? ItemNm { get; set; }
+    public string? PkgUnitCd { get; set; }
     public int Pkg { get; set; }
     public string QtyUnitCd { get; set; }
-    public decimal? Qty { get; set; }
-    public decimal? Prc { get; set; }
-    public decimal? SplyAmt { get; set; }
-    public decimal? TotDcAmt { get; set; }
-    public decimal? TaxblAmt { get; set; }
+    public float? Qty { get; set; }
+    public float? Prc { get; set; }
+    public float? SplyAmt { get; set; }
+    public int? TotDcAmt { get; set; }
+    public float? TaxblAmt { get; set; }
     public string VatCatCd { get; set; }
-    public decimal? TaxAmt { get; set; }
+    public float? TaxAmt { get; set; }
 }
