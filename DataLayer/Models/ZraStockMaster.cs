0290535a1adca20c9ed23413f008fe19f828a55e
Nigelo <vectorcrael@gmail.com>
2024-12-28 19:46:45 +0200

    remove warnings using data annotation.

--- DataLayer/Models/ZraStockMaster.cs
+++ DataLayer/Models/ZraStockMaster.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DataLayer.Models;
 
@@ -12,13 +13,16 @@ public class ZraStockMaster
     public string? ItemTypeCode { get; set; }
     public string? OriginNationCode { get; set; }
     public string? Description { get; set; }
+    [Column(TypeName = "decimal(18, 4)")]
     public decimal Quantity { get; set; }
     public string? TaxLabel { get; set; }
     public string? BranchId { get; set; }
     public double? Prc { get; set; } = 0;
     public double? SplyAmt { get; set; } = 0;
+    [Column(TypeName = "decimal(18, 4)")]
     public decimal? TaxblAmt { get; set; } = 0;
     public string? VatCatCd { get; set; } = "";
+    [Column(TypeName = "decimal(18, 4)")]
     public decimal? TaxAmt { get; set; } = 0;
     public double? TotAmt { get; set; } = 0;
 }
